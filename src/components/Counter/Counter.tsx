3c22b9770a5e5ac45906bdce449ef2d89083f6cb
“Marijn <marijn.stammeleer@springbokagency.com>
2022-12-03 00:48:49 +0100

    counter oplossing

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,34 +1,46 @@
-import React from 'react'
-import { Container, Controls, Reset, CustomInput } from './Counter.styles';
-
+import React, { useEffect, useRef } from "react";
+import { Container, Controls, Reset, CustomInput } from "./Counter.styles";
 
 const intialCount = 12;
 
 const Counter: React.FC = () => {
+  const [count, setCount] = React.useState<number>(intialCount);
 
-    const [count, setCount] = React.useState<number>(intialCount)
+  const handleControlChange = (e: React.MouseEvent) => {
+    setCount(count + +e.currentTarget.innerHTML);
+  };
 
-    return (
-        <Container>
-            <div>
-                <p>{count}</p>
-            </div>
-            <Controls>
-                <button> -5 </button>
-                <button> -1 </button>
-                <button> +1 </button>
-                <button> +5 </button>
-            </Controls>
-            <Reset>RESET</Reset>
-            <CustomInput>
-                <input type='number'/>
-                <button>ADD</button>
-            </CustomInput>
-        </Container>
-    )
-}
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleCustomChange = () => {
+    if (inputRef.current?.value) {
+      setCount(count + +inputRef.current?.value);
+    }
+  };
 
+  useEffect(() => {
+    if (count % 11 === 0) {
+      setCount(0);
+    }
+  }, [count]);
+
+  return (
+    <Container>
+      <div>
+        <p>{count}</p>
+      </div>
+      <Controls>
+        <button onClick={handleControlChange}> -5 </button>
+        <button onClick={handleControlChange}> -1 </button>
+        <button onClick={handleControlChange}> +1 </button>
+        <button onClick={handleControlChange}> +5 </button>
+      </Controls>
+      <Reset onClick={() => setCount(intialCount)}>RESET</Reset>
+      <CustomInput>
+        <input type="number" ref={inputRef} />
+        <button onClick={handleCustomChange}>ADD</button>
+      </CustomInput>
+    </Container>
+  );
+};
 
 export default Counter;
-
-
