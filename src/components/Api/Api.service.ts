32f9e3cf6e6d0e546e71dce46c73889cb3e80acf
“Marijn <marijn.stammeleer@springbokagency.com>
2022-12-03 02:59:40 +0100

    api interaction oplossing

--- src/components/Api/Api.service.ts
+++ src/components/Api/Api.service.ts
@@ -1,22 +1,26 @@
-import axios from 'axios'
+import axios, { AxiosError, AxiosResponse } from "axios";
 
-const ENDPOINT = 'https://randomuser.me/api?results=5'
+const ENDPOINT = "https://randomuser.me/api?results=5";
 
 export interface User {
-    gender: string
-    name: {
-        title: string,
-        first: string,
-        last: string,
-    },
-    picture: {
-        thumbnail: string,
-    }
+  gender: string;
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  picture: {
+    thumbnail: string;
+  };
 }
 
 const GetRandomUser = async () => {
-    //
-}
-
+  try {
+    const users = await axios.get(ENDPOINT);
+    return users.data.results;
+  } catch (error) {
+    console.error("error", error);
+  }
+};
 
 export default GetRandomUser;
--- src/components/Api/Api.tsx
+++ src/components/Api/Api.tsx
@@ -1,25 +1,36 @@
-import React from 'react'
-import GetRandomUser, { User } from './Api.service';
-import { Container, DataList, UserContainer } from './Api.styles';
+import React from "react";
+import GetRandomUser, { User } from "./Api.service";
+import { Container, DataList, UserContainer } from "./Api.styles";
 
 const Api: React.FC = () => {
+  const [data, setData] = React.useState([] as Array<User>);
 
-    const [data, setData] = React.useState([] as Array<User>)
+  const getUsers = async () => {
+    const users: User[] = await GetRandomUser();
+    setData(users);
+  };
 
-    React.useEffect(() => {
-        
-    }, [])
+  React.useEffect(() => {
+    getUsers();
+  }, []);
 
-    return (
-        <Container>
-            <DataList>
-                {data.length > 0 ? 'Show userlist here' : <p>Fetching users...</p>}
-            </DataList>
-        </Container>
-    )
-}
-
+  return (
+    <Container>
+      <DataList>
+        {data.length > 0 ? (
+          data.map((user) => (
+            <UserContainer style={{ borderColor: user.gender === "male" ? "blue" : "red" }}>
+              {/* <p>{user.gender}</p> */}
+              <p>{user.name.title + " " + user.name.first + " " + user.name.last}</p>
+              <img src={user.picture.thumbnail} alt="user thumbnail" />
+            </UserContainer>
+          ))
+        ) : (
+          <p>Fetching users...</p>
+        )}
+      </DataList>
+    </Container>
+  );
+};
 
 export default Api;
-
-
